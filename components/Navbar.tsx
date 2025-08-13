fab3fad3420420a44122707338777390b4a95821
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-08-13 12:07:53 +0100

    added features

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,55 @@
-"use client"
+"use client";
 
-import { Trello } from "lucide-react"
-import { Button } from "./ui/button"
-import { SignInButton, SignUpButton } from "@clerk/nextjs"
+import { ArrowRight, Trello } from "lucide-react";
+import { Button } from "./ui/button";
+import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Navbar() {
-    return(
-      <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50" >
-        <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
-            <div className="flex items-center space-x-2">
-                <Trello className="h-6 w-6 sm:h-8 text-blue-600" />
-                <span className="text-xl sm:text-2xl font-bold text-gray-900"> Atlas Provisions</span>
-            </div>
-            <div className="flex items-center space-x-2 sm:space-x-4">
-                <div>
-                    <SignInButton>
-                        <Button  variant="ghost" size="sm" className="text-xs sm:text-sm">
-                            Sign In
-                        </Button>
-                    </SignInButton>
-                    <SignUpButton>
-                         <Button>
-                            Sign Up
-                        </Button>
-                    </SignUpButton>
-                </div>
-
-            </div>
+  const { isSignedIn, user } = useUser();
+  return (
+    <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+      <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <Trello className="h-6 w-6 sm:h-8 text-blue-600" />
+          <span className="text-xl sm:text-2xl font-bold text-gray-900">
+            {" "}
+            Atlas Provisions
+          </span>
         </div>
-      </header>
-    )
+        <div className="flex items-center space-x-2 sm:space-x-4">
+          {isSignedIn ? (
+            <div className="flex flex-col sm:flex-row items-end sm:items-center space-y-7 sm:space-y-0 sm:space-x-4">
+              <span className="text-xs sm:text-sm text-gray-600 hidden sm:block ">
+                Welcome {user.firstName ??
+                user.emailAddresses[0].emailAddress}
+              </span>
+              <Link href="/dashboard">
+              <Button>
+                Go to Dashboard <ArrowRight />
+              </Button>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <div>
+                <SignInButton>
+                  <Button
+                    variant="ghost"
+                    size="sm"
+                    className="text-xs sm:text-sm"
+                  >
+                    Sign In
+                  </Button>
+                </SignInButton>
+                <SignUpButton>
+                  <Button>Sign Up</Button>
+                </SignUpButton>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
